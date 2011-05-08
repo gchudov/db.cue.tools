56b301701cba4180eb51f5b8fb538a3d47849f94
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-05-08 08:57:22 +0400

    Musicbrainz NGS

--- clones4.php
+++ clones4.php
@@ -3,17 +3,20 @@ include 'logo_start.php';
 require_once( 'phpctdb/ctdb.php' );
 
 $count = 50;
-$query = "select a.* from submissions2 a, submissions2 b where substring(a.trackoffsets from '(.*) ([^ ]*)$') = substring(b.trackoffsets from '(.*) ([^ ]*)$') AND (substring(a.trackoffsets from ' ([^ ]*)$')=int4( substring(b.trackoffsets from ' ([^ ]*)$')) + 150 OR substring(b.trackoffsets from ' ([^ ]*)$')=int4( substring(a.trackoffsets from ' ([^ ]*)$')) + 150)";
-$result = pg_query(str_replace('select a.*','select count(a.*)', $query));
-$total = pg_fetch_row($result);
-$total = $total[0];
-pg_free_result($result);
-$url = '';
 $start = @$_GET['start'] == '' ? 0 : @$_GET['start'];
-$query = $query . " ORDER BY trackoffsets OFFSET " . pg_escape_string($start) . " LIMIT " . pg_escape_string($count);
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+pg_query("BEGIN TRANSACTION; DECLARE curs CURSOR FOR " . 
+"select a.* from submissions2 a, submissions2 b where substring(a.trackoffsets from '(.*) ([^ ]*)$') = substring(b.trackoffsets from '(.*) ([^ ]*)$') AND (substring(a.trackoffsets from ' ([^ ]*)$')=int4( substring(b.trackoffsets from ' ([^ ]*)$')) + 150 OR substring(b.trackoffsets from ' ([^ ]*)$')=int4( substring(a.trackoffsets from ' ([^ ]*)$')) + 150) ORDER BY trackoffsets;") or die('Query failed: ' . pg_last_error());
+$result = pg_query("MOVE FORWARD " . pg_escape_string($start) . " IN curs") or die('Query failed: ' . pg_last_error());
+pg_free_result($result);
+$result = pg_query(" FETCH " . pg_escape_string($count) . " FROM curs") or die('Query failed: ' . pg_last_error());
+$result1 = pg_query("MOVE FORWARD ALL IN curs") or die('Query failed: ' . pg_last_error());
+$total = $start + pg_num_rows($result) + pg_affected_rows($result1);
+pg_free_result($result1);
+pg_query("COMMIT TRANSACTION") or die('Query failed: ' . pg_last_error());
+$url = '';
 if (pg_num_rows($result) == 0)
   die('nothing found');
+//$total += pg_cmdtuples($result);
 
 printf("<center><h3>Clones (%d):</h3>", $total);
 include 'list.php';
