4c3d8197d036bbb7a97da7234945430063e2996d
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-05-23 21:44:53 +0000

    backup timestamp fixed, images shrinked

--- dobackup.sh
+++ dobackup.sh
@@ -1,3 +1,3 @@
 cd /var/www/ctdbweb
-pg_dump -Fc ctdb -U ctdb_user > "backups/ctdbdump-`date +%Y-%m-%d`.bin"
-tar --gzip --create --file="backups/ctdbdata-`date +%Y-%m-%d`.tgz" --listed-incremental=backups/ctdbdata.snar parity
+pg_dump -Fc ctdb -U ctdb_user > "backups/ctdbdump-`date +%s-%Y-%m-%d`.bin"
+tar --gzip --create --file="backups/ctdbdata-`date +%s-%Y-%m-%d`.tgz" --listed-incremental=backups/ctdbdata.snar parity
