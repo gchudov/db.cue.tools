867cdeb60672eca741e49e6a854042a1a20d2fcd
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-05-25 07:32:02 +0000

    top.php also uses gvis now

--- clones4.php
+++ clones4.php
@@ -1,11 +1,11 @@
 <?php
-include 'logo_start.php'; 
+include 'logo_start1.php'; 
 require_once( 'phpctdb/ctdb.php' );
 
 $count = 50;
 $start = @$_GET['start'] == '' ? 0 : @$_GET['start'];
 pg_query("BEGIN TRANSACTION; DECLARE curs CURSOR FOR " . 
-"select a.* from submissions2 a, submissions2 b where substring(a.trackoffsets from '(.*) ([^ ]*)$') = substring(b.trackoffsets from '(.*) ([^ ]*)$') AND (substring(a.trackoffsets from ' ([^ ]*)$')=int4( substring(b.trackoffsets from ' ([^ ]*)$')) + 150 OR substring(b.trackoffsets from ' ([^ ]*)$')=int4( substring(a.trackoffsets from ' ([^ ]*)$')) + 150) ORDER BY trackoffsets;") or die('Query failed: ' . pg_last_error());
+"select a.* from submissions2 a, submissions2 b where substring(a.trackoffsets from '(.*) ([^ ]*)$') = substring(b.trackoffsets from '(.*) ([^ ]*)$') AND (int4(substring(a.trackoffsets from ' ([^ ]*)$')) = int4(substring(b.trackoffsets from ' ([^ ]*)$')) + 150 OR int4(substring(b.trackoffsets from ' ([^ ]*)$')) = int4(substring(a.trackoffsets from ' ([^ ]*)$')) + 150) ORDER BY trackoffsets;") or die('Query failed: ' . pg_last_error());
 $result = pg_query("MOVE FORWARD " . pg_escape_string($start) . " IN curs") or die('Query failed: ' . pg_last_error());
 pg_free_result($result);
 $result = pg_query(" FETCH " . pg_escape_string($count) . " FROM curs") or die('Query failed: ' . pg_last_error());
@@ -18,9 +18,10 @@ if (pg_num_rows($result) == 0)
   die('nothing found');
 //$total += pg_cmdtuples($result);
 
+include 'list1.php';
+include 'logo_start2.php';
 printf("<center><h3>Clones (%d):</h3>", $total);
-include 'list.php';
-pg_free_result($result);
+printf("<div id='entries_div'></div>\n");
 printf("</center>");
 ?>
 </body>
