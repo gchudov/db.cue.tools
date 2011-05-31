7ec47bf3ae0a9bd21bfa98c91446563e44b46ed0
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-05-31 23:09:55 +0000

    amazon s3 support

--- getdata.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename=ctdbdump.tar');
-header('Content-Transfer-Encoding: binary');
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-header('Pragma: public');
-passthru('tar c parity/');
-?>
--- utils/s3.php
+++ utils/s3.php
@@ -2,7 +2,7 @@
 error_reporting(-1);
 mb_internal_encoding("UTF-8");
 
-$bucket = 'parity-cuetools-net';
+$bucket = 'p.cuetools.net';
 
 require_once 'AWSSDKforPHP/sdk.class.php';
 require_once '../phpctdb/ctdb.php';
@@ -10,40 +10,54 @@ require_once '../phpctdb/ctdb.php';
 $dbconn = pg_connect("dbname=ctdb user=ctdb_user port=6543")
     or die('Could not connect: ' . pg_last_error());
 $s3 = new AmazonS3();
-
+$s3->set_region(AmazonS3::REGION_US_E1);
+//$s3->enable_path_style();
+$s3->disable_ssl();
+$s3->adjust_offset(60*60);
 while (true)
 {
 pg_query("BEGIN");
-$result = pg_query("SELECT * FROM submissions2 where parfile IS NOT NULL AND NOT s3 LIMIT 100")
+$result = pg_query("SELECT * FROM submissions2 where parfile IS NOT NULL AND NOT s3 LIMIT 10")
         or die('Query failed: ' . pg_last_error());
 $records = pg_fetch_all($result);
 pg_free_result($result);
 
 if (!$records || count($records) == 0) die("nothing to do\n");
 
+$ts = 0;
 foreach ($records as $record)
 {
+  $localname = '/var/www/ctdbweb/' . $record['parfile'];
+  if (!file_exists($localname)) {
+    echo 'File missing: ';
+    print_r($record);
+    $result = pg_query_params($dbconn, "UPDATE submissions2 SET parfile = NULL WHERE id=$1", array($record['id']));
+    pg_free_result($result);
+    continue;
+  }
+  $ts += filesize($localname);
   $crc32 = $record['crc32'];
   $tocid = $record['tocid'];
   $tocidsafe = str_replace('.','+',$tocid);
   $filename = sprintf("%s%08x", $tocidsafe, $crc32);
-  $localname = '/var/www/ctdbweb/' . $record['parfile'];
   $s3->batch()->create_object($bucket, $filename, array(
 	'fileUpload' => $localname,
 	'acl' => AmazonS3::ACL_PUBLIC
   ));
-  echo $localname . ' => ' . $filename . "\n";
+  //echo $localname . ' => ' . $filename . "\n";
 
   $result = pg_query_params($dbconn, "UPDATE submissions2 SET s3 = TRUE WHERE id=$1", array($record['id']));
   pg_free_result($result);
 }
+$start = microtime(true);
 $file_upload_response = $s3->batch()->send();
-
 if (!$file_upload_response->areOK())
 {
   pg_query("ABORT");
   die("abort\n");
 }
-
+$dur = microtime(true) - $start;
+if ($dur < 0.01) $dur = 0.01;
+printf("COMMIT %d files, %d bytes in %d secs (%dKB/s)\n", count($records), $ts, $dur, (int)($ts/$dur/1024));
 pg_query("COMMIT");
 }
