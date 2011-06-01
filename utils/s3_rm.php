66321bfcd5ca8b8bcac26e3c239197e3f82f95fe
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-06-01 00:12:24 +0000

    s3

--- utils/s3_rm.php
+++ utils/s3_rm.php
@@ -5,25 +5,16 @@ $bucket = 'parity-cuetools-net';
 require_once 'AWSSDKforPHP/sdk.class.php';
 
 $s3 = new AmazonS3();
+$queue = new $s3->batch_class(10);
 $s3->set_region(AmazonS3::REGION_US_E1);
 $s3->disable_ssl();
 //$s3->enable_path_style();
-$i = 0;
 do
 {
   $key = trim(fgets(STDIN));
   if (!$key || $key == '') break;
-  $s3->batch()->delete_object($bucket, $key);
-  $i++;
-  if ($i > 5) {
-    $file_upload_response = $s3->batch()->send();
-    if (!$file_upload_response->areOK()) die('oops');
-    echo $key . "\n";
-    $i = 0;
-  }
+  $s3->batch($queue)->delete_object($bucket, $key);
 } while (true);
-if ($i > 0) {
-    $file_upload_response = $s3->batch()->send();
-    if (!$file_upload_response->areOK()) die('oops');
-}
+$file_upload_response = $s3->batch($queue)->send();
+if (!$file_upload_response->areOK()) die('oops');
 
