094cf03a94899b49f2a9c76a6c2ede9a4afd0b8d
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-06-03 01:16:47 +0000

    use google vis in recent.php

--- recent.php
+++ recent.php
@@ -1,29 +1,8 @@
 <?php
-include 'logo_start.php'; 
-
-function agentIcon($agent)
-{
-  if (preg_match('/CUETools /', $agent))
-    return 'cuetools.png';
-  if (preg_match('/CUERipper /', $agent))
-    return 'cueripper.png';
-  if (preg_match('/EACv.* CTDB /', $agent))
-    return 'eac.png';
-  return '';
-}
-
-function agentName($agent)
-{
-  if (preg_match('/(CUETools|CUERipper|EACv.* CTDB) ([\d\.]*)/', $agent, $matches))
-    return $matches[2];
-  return $agent;
-}
+include 'logo_start1.php';
+require_once( 'phpctdb/ctdb.php' );
 
 if (!$isadmin) makeAuth1($realm, 'Admin priveleges required');
-
-printf("<center><h3>Recent additions:</h3>");
-include 'table_start.php';
-printf('<table width=100%% class=classy_table cellpadding=3 cellspacing=0><tr bgcolor=#D0D0D0><th>Date</th><th>Agent</th><th>Drive</th><th>User</th><th>Ip</th><th>Artist</th><th>Title</th><th>TOC Id</th><th>Tr.#</th><th>CTDB Id</th><th>AR</th></tr>' . "\n");
 
 $query = "";
 $params = false;
@@ -79,30 +58,116 @@ if ($query == '')
 	$show_date = false;
 }
 
-$result = pg_query_params($dbconn, "SELECT time, agent, drivename, userid, ip, s.entryid as entryid, s.confidence as confidence, e.confidence as confidence2, crc32, tocid, artist, title, firstaudio, audiotracks, trackcount FROM submissions s INNER JOIN submissions2 e ON e.id = s.entryid WHERE " . $query . " ORDER by s.subid DESC LIMIT 50", $params)
+$result = pg_query_params($dbconn, "SELECT time, agent, drivename, userid, ip, s.entryid as entryid, s.confidence as confidence, e.confidence as confidence2, crc32, tocid, artist, title, firstaudio, audiotracks, trackcount, trackoffsets FROM submissions s INNER JOIN submissions2 e ON e.id = s.entryid WHERE " . $query . " ORDER by s.subid DESC LIMIT 100", $params)
   or die('Query failed: ' . pg_last_error());
-while (TRUE == ($record3 = pg_fetch_array($result)))
-  printf('<tr><td class=td_discid>%s</td><td class=td_artist><img src="%s"> <a href="?agent=%s">%s</a></td><td class=td_discid><a href="?drivename=%s">%s</a></td><td class=td_discid><a href="?uid=%s">%.5s</a></td><td class=td_discid><a href="?ip=%s">%s</a></td><td class=td_artist><a href="?artist=%s">%s</a></td><td class=td_album>%s</td><td class=td_discid><a href="?tocid=%s">%.5s</a></td><td class=td_ar>%s</td><td class=td_ctdbid><a href="show.php?id=%d">%08x</td><td class=td_ar>%s</td></tr>' . "\n",
-    $show_date ? $record3['time'] : substr($record3['time'],11),
-    agentIcon($record3['agent']), $record3['agent'],  agentName($record3['agent']),
-    $record3['drivename'], mb_substr($record3['drivename'],0,28),
-    $record3['userid'],
-    $record3['userid'],
-		@$record3['ip'],
-		@$record3['ip'],
-		urlencode($record3['artist']),
-		mb_substr($record3['artist'],0,22),
-		mb_substr($record3['title'],0,32),
-		$record3['tocid'],
-		$record3['tocid'],
-		($record3['firstaudio'] > 1) ? ('1+' . $record3['audiotracks']) : (($record3['audiotracks'] < $record3['trackcount']) ? ($record3['audiotracks'] . '+1') : $record3['audiotracks']),
-		$record3['entryid'],
-		$record3['crc32'],
-		$record3['confidence'] == $record3['confidence2'] ? $record3['confidence'] : sprintf('%d/%d', $record3['confidence'], $record3['confidence2'])
-	);
+$submissions = pg_fetch_all($result);
 pg_free_result($result);
-printf("</table>");
-include 'table_end.php';
+
+$json_submissions = null;
+foreach($submissions as $record)
+{
+  $trcnt = ($record['firstaudio'] > 1) ?
+    ('1+' . $record['audiotracks']) :
+    (($record['audiotracks'] < $record['trackcount'])
+     ? ($record['audiotracks'] . '+1')
+     : $record['audiotracks']);
+  $json_submissions[] = array(
+    'c' => array(
+      array('v' => $show_date ? $record['time'] : substr($record['time'],11)),
+      array('v' => $record['agent'] ? $record['agent'] : ''),
+      array('v' => $record['drivename'] ? $record['drivename'] : ''),
+      array('v' => $record['userid'] ? $record['userid'] : ''),
+      array('v' => @$record['ip']),
+      array('v' => $record['artist']),
+      array('v' => $record['title']),
+      array('v' => $record['tocid']),
+      array('v' => $trcnt),
+      array('v' => (int)$record['entryid']),
+      array('v' => $record['confidence'] == $record['confidence2'] ? $record['confidence'] : sprintf('%d/%d', $record['confidence'], $record['confidence2'])),
+      array('v' => (int)$record['crc32']),
+      array('v' => phpCTDB::toc2mbid($record)),
+    ));
+}
+$json_submissions_table = array(
+  'cols' => array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'Agent', 'type' => 'string'),
+    array('label' => 'Drive', 'type' => 'string'),
+    array('label' => 'User', 'type' => 'string'),
+    array('label' => 'IP', 'type' => 'string'),
+    array('label' => 'Artist', 'type' => 'string'),
+    array('label' => 'Album', 'type' => 'string'),
+    array('label' => 'TOC Id', 'type' => 'string'),
+    array('label' => 'Tr#', 'type' => 'string'),
+    array('label' => 'CTDB Id', 'type' => 'number'),
+    array('label' => 'AR', 'type' => 'string'),
+    array('label' => 'CRC32', 'type' => 'number'),
+    array('label' => 'MB Id', 'type' => 'string'),
+    ),
+  'rows' => $json_submissions);
+$json_submissions = json_encode($json_submissions_table);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript" src="https://www.google.com/jsapi?autoload=%7B%22modules%22%3A%5B%7B%22name%22%3A%22visualization%22%2C%22version%22%3A%221%22%2C%22packages%22%3A%5B%22table%22%5D%7D%5D%7D"></script>
+<script type='text/javascript'>
+google.setOnLoadCallback(drawTable);
+function ctdbEntryData(json)
+{
+  function decimalToHexString(number)
+  {
+    if (number < 0)
+    {
+        number = 0xFFFFFFFF + number + 1;
+    }
+    var hex = number.toString(16).toUpperCase();
+    return "00000000".substr(0, 8 - hex.length) + hex;
+  }
+
+  var data = new google.visualization.DataTable(json);
+  for (var row = 0; row < data.getNumberOfRows(); row++) {
+    data.setProperty(row, 0, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    data.setProperty(row, 1, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    var matches = data.getValue(row, 1).match(/(CUETools|CUERipper|EACv.* CTDB) ([\d\.]*)/);
+    var img = matches[1] == 'CUETools' ? 'cuetools.png' :  matches[1] == 'CUERipper' ? 'cueripper.png' : matches[1] == 'EACv1.0b2 CTDB' ? 'eac.png' : ''; 
+    data.setFormattedValue(row, 1, (img != '' ? '<img height=12 src="' + img + '">' : '') + '<a href="?agent=' + data.getValue(row, 1) + '">' + matches[2] + '</a>');
+    data.setProperty(row, 2, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    data.setFormattedValue(row, 2, '<a href="?drive=' + data.getValue(row, 2) + '">' + data.getValue(row, 2).substring(0,20) + '</a>');
+    data.setProperty(row, 3, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    data.setFormattedValue(row, 3, '<a href="?uid=' + data.getValue(row, 3) + '">' + data.getValue(row, 3).substring(0,6) + '</a>');
+    data.setProperty(row, 4, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    var artist = data.getValue(row, 5);
+    if (!artist) artist = "Unknown Artist";
+    data.setFormattedValue(row, 5, '<a href="?artist=' + encodeURIComponent(artist) + '">' + artist.substring(0,30) + '</a>');
+    var title = data.getValue(row, 6);
+    if (!title) title = "Unknown Title";
+    data.setFormattedValue(row, 6, title.substring(0,30));
+    data.setProperty(row, 7, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    var toc = data.getValue(row, 7);
+    data.setFormattedValue(row, 7, '<a href="?tocid=' + toc + '">' + toc.substring(0,7) + '</a>');
+    data.setProperty(row, 8, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    data.setProperty(row, 9, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+    data.setFormattedValue(row, 9, '<a href="show.php?id=' + data.getValue(row, 9).toString(10) + '">' + decimalToHexString(data.getValue(row, 11)) + '</a>');
+    data.setProperty(row, 10, 'className', 'google-visualization-table-td google-visualization-table-td-consolas');
+  }
+  return data;
+}
+function drawTable()
+{
+  var data = ctdbEntryData(<?php echo $json_submissions;?>);
+  var table = new google.visualization.Table(document.getElementById('entries_div'));
+  var opts = {allowHtml: true, width: 1200, sort: 'disable', showRowNumber: false, page: 'enable', pageSize: 20};
+  var view = new google.visualization.DataView(data);
+  view.hideColumns([11,12]);
+  table.draw(view, opts);
+}
+</script>
+
+<?php
+include 'logo_start2.php';
+printf("<center><h3>Recent additions:</h3>");
+printf("<div id='entries_div'></div>\n");
 printf("</center>");
 ?>
 </body>
