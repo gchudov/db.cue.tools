ace42ad7c2b594eb57cd250ec5a7346fd4aec622
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-06-21 00:51:54 +0000

    using XML_Serializer

--- logo_start2.php
+++ logo_start2.php
@@ -12,10 +12,10 @@ pg_free_result($dbresult);
   <li><img width=64 height=64 border=0 alt="" src="http://s3.cuetools.net/ctdb64.png"></li>
   <li id="nav-1"><a href="/">Home</a></li>
   <li id="nav-2"><a href="/top.php">Popular</a></li>
+  <li id="nav-6"><a href="/stats.php">Stats</a></li>
   <li id="nav-3"><a href="http://www.cuetools.net/wiki/CUETools_Database" target="_blank">About</a></li>
 	<li id="nav-4"><a href="http://www.hydrogenaudio.org/forums/index.php?showtopic=79882" target="_blank">Forum</a></li>
 	<?php if ($isadmin) { ?><li id="nav-6"><a href="/recent.php">Recent</a></li><?php }?>
-	<?php if ($isadmin) { ?><li id="nav-6"><a href="/stats.php">Stats</a></li><?php }?>
 	<?php if ($isadmin) { ?><li id="nav-7"><a href="/?logout=1">Logout</a></li><?php }?>
 	<li id="nav-8"><a href="http://www.cuetools.net/wiki/CTDB_EAC_Plugin" target="_blank">EAC Plugin</a></li>
 	<li id="nav-0"><a href="http://www.cuetools.net/wiki/CUETools" target="_blank">CUETools</a></li>
---
+++ lookup3.php
@@ -0,0 +1,106 @@
+<?php
+require_once 'phpctdb/ctdb.php';
+require_once 'XML/Serializer.php';
+
+$dbconn = pg_connect("dbname=ctdb user=ctdb_user port=6543") or die('Could not connect: ' . pg_last_error());
+$toc_s = $_GET['toc'] or die('Invalid arguments');
+$dometa = @$_GET['musicbrainz'];
+$dofreedb = @$_GET['freedb'];
+$fuzzy = @$_GET['fuzzy'];
+$toc = phpCTDB::toc_s2toc($toc_s);
+$tocid = phpCTDB::toc2tocid($toc); 
+$query = "SELECT * FROM submissions2 WHERE tocid='" . pg_escape_string($tocid) . "'";
+if (!$fuzzy) $query = $query . " AND trackoffsets='" . pg_escape_string($toc['trackoffsets']) . "'";
+$result = pg_query($dbconn, $query) 
+  or die('Query failed: ' . pg_last_error());
+
+$records = pg_fetch_all($result);
+pg_free_result($result);
+$mbids = false;
+if (@$toc)
+  $mbids[] = phpCTDB::toc2mbid($toc);
+if ($records && $fuzzy)
+  foreach($records as $record)
+    $mbids[] = phpCTDB::toc2mbid($record);
+
+$mbmetas = false;
+if ($dometa)
+foreach (array_unique($mbids) as $mbid)
+{
+  $mbmeta_t = phpCTDB::mblookup($mbid);
+  if ($mbmeta_t)
+    foreach ($mbmeta_t as $im)
+      $mbmetas[] = $im;
+}
+if ($dofreedb)
+{
+  $mbmeta_t = phpCTDB::freedblookup($toc_s);
+  if ($mbmeta_t)
+    foreach ($mbmeta_t as $im)
+      $mbmetas[] = $im;
+}
+if (!$records && !$mbmetas)
+{
+  ob_clean();
+  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found"); 
+  header("Status: 404 Not Found");
+  die('Not Found'); 
+}
+
+$xmlentry = false;
+$i = 0;
+if ($records)
+foreach($records as $record)
+{
+  $xmlentry[] = array(
+    'id' => $record['id'],
+    'crc32' => sprintf("%08x", $record['crc32']),
+    'confidence' => $record['confidence'], 
+    'npar' => 8, 
+    'stride' => 5880,
+    'hasparity' => ($record['s3'] == 't' ? sprintf("http://p.cuetools.net/%s%08x", str_replace('.','%2B',$record['tocid']), $record['crc32']) :  ($record['parfile'] ? "/" . $record['parfile'] : false)),
+    'parity' => $record['parity'],
+    'toc' => phpCTDB::toc_toc2s($record)
+  );
+}
+$xmlmbmeta = false; 
+if ($mbmetas)
+foreach ($mbmetas as $mbmeta)
+{
+  $tracks = null;
+  foreach ($mbmeta['tracklist'] as $track) {
+    if ($track['artist'] == $mbmeta['artistname'])
+      $track['artist'] = null;
+    $tracks[] = $track;
+  }
+  $xmlmbmeta[] = array(
+    'release_gid' => $mbmeta['gid'],
+    'artist' => $mbmeta['artistname'],
+    'album' => $mbmeta['albumname'],
+    'year' => $mbmeta['first_release_date_year'], 
+    'releasedate' => $mbmeta['releasedate'], 
+    'country' => $mbmeta['country'], 
+    'discnumber' => $mbmeta['discnumber'], 
+    'disccount' => $mbmeta['totaldiscs'], 
+    'discname' => $mbmeta['discname'], 
+    'coverarturl' => $mbmeta['coverarturl'], 
+    'infourl' => $mbmeta['info_url'], 
+    'barcode' => $mbmeta['barcode'],
+    'track' => $tracks,
+    'label' => @$mbmeta['label'],
+  );
+}
+$ctdbdata = array('entry' => $xmlentry, 'musicbrainz' => $xmlmbmeta);
+header('Content-type: text/xml; charset=UTF-8');
+$options = array(
+  XML_SERIALIZER_OPTION_INDENT        => '    ',
+  XML_SERIALIZER_OPTION_RETURN_RESULT => true,
+  XML_SERIALIZER_OPTION_SCALAR_AS_ATTRIBUTES => true,
+  XML_SERIALIZER_OPTION_MODE          => XML_SERIALIZER_MODE_SIMPLEXML,
+  XML_SERIALIZER_OPTION_IGNORE_NULL   => true,
+  XML_SERIALIZER_OPTION_ROOT_NAME     => 'ctdb',
+  XML_SERIALIZER_OPTION_ROOT_ATTRIBS  => array('xmlns'=>"http://db.cuetools.net/ns/mmd-1.0#", 'xmlns:ext'=>"http://db.cuetools.net/ns/ext-1.0#"),
+  XML_SERIALIZER_OPTION_XML_ENCODING  => 'UTF-8'
+  );
+$serializer = &new XML_Serializer($options);
+echo $serializer->serialize($ctdbdata);
