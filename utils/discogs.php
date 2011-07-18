ab754367654c98cdb03f08a9287728eacbfe2f80
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-07-18 19:43:09 +0000

    discogs import: more fields supported

--- utils/discogs.php
+++ utils/discogs.php
@@ -13,20 +13,84 @@ function escapeNodes($nodes)
   return "ARRAY[" . substr($res,1) . "]";
 }
 
+function printInsert($table, $record)
+{
+  $keys = '';
+  $vals = '';
+  foreach ($record as $key => $val) {
+    $keys .= ",$key";
+    $vals .= ",$val";
+  }
+  $keys = substr($keys,1);
+  $vals = substr($vals,1);
+  echo "INERT INTO $table($keys) VALUES($vals);\n";
+}
+
 function parseRelease($rel)
 {
   //print_r( $rel);
-  $discogs_id = $rel['id'];
-  $title = escapeNode($rel->title);
-  $status = escapeNode($rel['status']);
-  $country = escapeNode($rel->country);
-  $released = escapeNode($rel->released);
-  $notes = escapeNode($rel->notes);
-  $genres = escapeNodes($rel->genres);
-  $styles = escapeNodes($rel->styles);
-
-  echo "INSERT INTO release(discogs_id, title, status, country, released, notes, genres, styles) " . 
-    "VALUES($discogs_id,$title,$status,$country,$released,$notes,$genres,$styles);\n";
+  printInsert('release', array(
+    'discogs_id' => $rel['id'],
+    'title' => escapeNode($rel->title),
+    'status' => escapeNode($rel['status']),
+    'country' => escapeNode($rel->country),
+    'released' => escapeNode($rel->released),
+    'notes' => escapeNode($rel->notes),
+    'genres' => escapeNodes($rel->genres),
+    'styles' => escapeNodes($rel->styles)));
+  if ($rel->labels)
+  foreach($rel->labels->children() as $lbl) {
+    printInsert('releases_labels', array(
+      'discogs_id' => $rel['id'],
+      'label' => escapeNode($lbl['name']),
+      'catno' => escapeNode($lbl['catno'])));
+  }
+  $seq = 0;
+  if ($rel->artists)
+  foreach($rel->artists->children() as $art) {
+    printInsert('releases_artists', array(
+      'discogs_id' => $rel['id'],
+      'seq' => $seq++,
+      'name' => escapeNode($art->name),
+      'anv' => escapeNode($art->anv),
+      'join' => escapeNode($art->join),
+      'role' => escapeNode($art->role),
+      'tracks' => escapeNode($art->tracks)));
+  }
+/*
+  $seq = 0;
+  if ($rel->extraartists)
+  foreach($rel->extraartists->children() as $art) {
+    printInsert('releases_extraartists', array(
+      'discogs_id' => $rel['id'],
+      'seq' => $seq++,
+      'name' => escapeNode($art->name),
+      'anv' => escapeNode($art->anv),
+      'join' => escapeNode($art->join),
+      'role' => escapeNode($art->role),
+      'tracks' => escapeNode($art->tracks)));
+  }
+*/
+  if ($rel->formats)
+  foreach($rel->formats->children() as $fmt) {
+    printInsert('releases_formats', array(
+      'discogs_id' => $rel['id'],
+      'format_name' => escapeNode($fmt['name']),
+      'qty' => $fmt['qty'],
+      'descriptions' => escapeNodes($fmt->descriptions)));
+  }
+  if ($rel->images)
+  foreach($rel->images->children() as $img) {
+    printInsert('image', array(
+      'uri' => escapeNode($img['uri']),
+      'height' => $img['height'],
+      'width' => $img['width'],
+      'type' => escapeNode($img['type']),
+      'uri150' => escapeNode($img['uri150'])));
+    printInsert('releases_images', array(
+      'discogs_id' => $rel['id'],
+      'image_uri' => escapeNode($img['uri'])));
+  }
 }
 
 $xml = new XMLReader();
