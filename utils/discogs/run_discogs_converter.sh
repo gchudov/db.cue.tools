2b97aa22952c9aa932e0cb7ce6698fd4c3360624
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-07-23 02:15:50 +0000

    fix wierd symbols

--- utils/discogs/run_discogs_converter.sh
+++ utils/discogs/run_discogs_converter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gunzip -c | sed 's/[\x01-\x08|\x0B|\x0C|\x0E-\x1F]//g' | php `dirname $0`/discogs.php | gzip > discogs_enums_sql.gz
-(cat `dirname $0`/create_tables.sql ; gunzip -c discogs_*_sql.gz ; cat `dirname $0`/create_keys.sql) | bzip2 -1
+gunzip -c | sed -e 's/[\x01-\x08|\x0B|\x0C|\x0E-\x1F]//g' -e 's/33 â[^,}]+ RPM/33 ⅓ RPM/g' | php `dirname $0`/discogs.php | gzip > discogs_enums_sql.gz
+(cat `dirname $0`/create_tables.sql ; gunzip -c discogs_*_sql.gz ; cat `dirname $0`/create_keys.sql) | bzip2 -2
