ced3e7f2d906efb92b25fda63b681d6e150bffe0
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-07-23 23:38:42 +0000

    discogs: first_release_date

--- utils/discogs/create_keys.sql
+++ utils/discogs/create_keys.sql
@@ -49,6 +49,8 @@ CREATE INDEX track_release_id_index ON track(release_id);
 
 CREATE INDEX releases_labels_release_id_index ON releases_labels(release_id);
 
+CREATE INDEX release_master_id_index ON release(master_id);
+
 --
 -- Name: artist_credit_name_anv_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
