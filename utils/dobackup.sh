b86ec10dd3d2fe0dbfa56b2916f824136b3714e8
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-10-11 01:33:46 +0000

    backup everything

--- utils/dobackup.sh
+++ utils/dobackup.sh
@@ -1,4 +1,13 @@
+backup_path="s3://private.cuetools.net/backups/`date +%Y-%m-%d-%s`/"
 pg_dump -Fc ctdb -U ctdb_user > /tmp/ctdb.bin
-s3cmd put /tmp/ctdb.bin "s3://private.cuetools.net/backups/ctdbdump-`date +%s-%Y-%m-%d`.bin"
+s3cmd put --rr --no-progress /tmp/ctdb.bin $backup_path
 rm /tmp/ctdb.bin
-#tar --gzip --create --file="backups/ctdbdata-`date +%s-%Y-%m-%d`.tgz" --listed-incremental=backups/ctdbdata.snar parity
+tar --gzip --create --file="/tmp/ctdb.tgz" /var/www/ctdbweb --exclude="/var/www/ctdbweb/parity/*"
+s3cmd put --rr --no-progress /tmp/ctdb.tgz $backup_path
+rm /tmp/ctdb.tgz
+pg_dump -Fc wikidb -U postgres > /tmp/wiki.bin
+s3cmd put --rr --no-progress /tmp/wiki.bin $backup_path
+rm /tmp/wiki.bin
+tar --gzip --create --file="/tmp/wiki.tgz" /var/www/w/images /var/www/w/extensions /var/www/w/skins /var/www/w/LocalSettings.php
+s3cmd put --rr --no-progress /tmp/wiki.tgz $backup_path
+rm /tmp/wiki.tgz
