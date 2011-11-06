9fadcca57f13f60d449800679e5e527d51871aa5
Gregory S. Chudov <Gregory S. Chudov@localhost>
2011-11-06 20:06:27 +0000

    CTDB 2.0

--- submit2.php
+++ submit2.php
@@ -1,31 +1,65 @@
 <?php
 require_once( 'phpctdb/ctdb.php' );
+require_once 'XML/Serializer.php';
 
 //if ($_SERVER['HTTP_USER_AGENT'] != "CUETools 205")
-//  die ("user agent " . $_SERVER['HTTP_USER_AGENT'] . " is not allowed");
+//  fatal_error("user agent " . $_SERVER['HTTP_USER_AGENT'] . " is not allowed");
+
+$options = array(
+  XML_SERIALIZER_OPTION_INDENT        => '  ',
+  XML_SERIALIZER_OPTION_RETURN_RESULT => true,
+  XML_SERIALIZER_OPTION_SCALAR_AS_ATTRIBUTES => true,
+  XML_SERIALIZER_OPTION_MODE          => XML_SERIALIZER_MODE_SIMPLEXML,
+  XML_SERIALIZER_OPTION_IGNORE_NULL   => true,
+  XML_SERIALIZER_OPTION_ROOT_NAME     => 'ctdb',
+  XML_SERIALIZER_OPTION_ROOT_ATTRIBS  => array('xmlns'=>"http://db.cuetools.net/ns/mmd-1.0#", 'xmlns:ext'=>"http://db.cuetools.net/ns/ext-1.0#"),
+  XML_SERIALIZER_OPTION_XML_ENCODING  => 'UTF-8'
+  );
+$serializer = &new XML_Serializer($options);
+
+$version = isset($_POST['ctdb']) ? $_POST['ctdb'] : 2;
+if ($version == 2) header('Content-type: text/xml; charset=UTF-8');
+
+function report_success($reason) {
+  global $version, $serializer;
+  $response = array('status' => 'success', 'message' => $reason);
+  die($version == 1 ? $reason : $serializer->serialize($response));
+}
+
+function fatal_error($reason) {
+  global $version, $serializer;
+  $response = array('status' => 'error', 'message' => $reason);
+  die($version == 1 ? $reason : $serializer->serialize($response));
+}
+
+function parity_needed($npar) {
+  global $version, $serializer;
+  die($version == 1 ? 'parity needed' : $serializer->serialize(array('status' => 'parity needed', 'message' => 'parity needed', 'npar' => $npar)));
+}
 
 $dbconn = pg_connect("dbname=ctdb user=ctdb_user port=6543")
-    or die('Could not connect: ' . pg_last_error());
+  or fatal_error('Could not connect: ' . pg_last_error());
 
 $confirmid = @$_POST['confirmid'];
 
 $toc_s = @$_POST['toc'];
-if (!$toc_s) die('toc not specified');
+if (!$toc_s) fatal_error('toc not specified');
 
 $toc = phpCTDB::toc_s2toc($toc_s);
 $tocid = phpCTDB::toc2tocid($toc);
 
 $paritysample = @$_POST['parity'];
-if (!$paritysample) die('parity not specified');
+if (!$paritysample) fatal_error('parity not specified');
+$syndromesample = @$_POST['syndrome'];
 
 $crc32 = $_POST['crc32'];
-if (!$crc32) die('crc32 not specified');
+if (!$crc32) fatal_error('crc32 not specified');
 
 $trackcrcs = $_POST['trackcrcs'];
-if (!$trackcrcs) die('trackcrcs not specified');
+if (!$trackcrcs) fatal_error('trackcrcs not specified');
 
 $confidence = $_POST['confidence'];
-if (!$confidence) die('confidence not specified');
+if (!$confidence) fatal_error('confidence not specified');
 
 $quality = $_POST['quality'];
 
@@ -33,10 +67,10 @@ if (@$_POST['parityfile'])
 {
   $file = $_FILES['parityfile'];
   if ($file['error'])
-    die("Error " . $file['error'] . "; upload_max_filesize " . ini_get('upload_max_filesize'));
+    fatal_error($file['error'] . "; upload_max_filesize " . ini_get('upload_max_filesize'));
   $tmpname = $file['tmp_name'];
-  @file_exists($tmpname) or die("file doesn't exist");
-  if (filesize($tmpname) == 0) die("file is empty");
+  @file_exists($tmpname) or fatal_error("file doesn't exist");
+  if (filesize($tmpname) == 0) fatal_error("file is empty");
   $parfile = true;
 } else {
   $tmpname = false;
@@ -56,18 +90,19 @@ if (isset($_POST['barcode'])) $record3['barcode'] = $_POST['barcode'];
 function submit_error($dbconn, $submission, $reason) {
   $submission['reason'] = $reason;
   pg_insert($dbconn, 'failed_submissions', $submission);
-  die($reason);
+  fatal_error($reason);
 }
 
 $needparfile = false;
 if ($confirmid)
 { 
   $result = pg_query_params($dbconn, "SELECT * FROM submissions2 WHERE id=$1", array($confirmid))
-    or die('Query failed: ' . pg_last_error());
+    or fatal_error('Query failed: ' . pg_last_error());
   $oldrecord = pg_fetch_array($result)
-    or die('Query failed: ' . pg_last_error());
+    or fatal_error('Query failed: ' . pg_last_error());
   pg_free_result($result);
   if ($oldrecord['hasparity'] != 't') $needparfile = true;
+  if ($oldrecord['syndrome'] == null && $syndromesample != null) $needparfile = true;
   @$oldrecord['trackcrcs'] or $needparfile = true;
 }
 else
@@ -80,13 +115,13 @@ else
 if ($parfile && !$needparfile)
   submit_error($dbconn, $record3, "parity not needed"); // parfile = false?
 if (!$parfile && $needparfile)
-  die ('parity needed');
+  parity_needed(16);
 
 if ($confirmid) {
   $record3['entryid'] =  $confirmid;
 
   $result = pg_query_params($dbconn, "SELECT * FROM submissions WHERE entryid=$1 AND (userid=$2 OR ip=$3) AND drivename=$4", array($confirmid, $record3['userid'], $record3['ip'], $record3['drivename']))
-    or die('Query failed: ' . pg_last_error());
+    or fatal_error('Query failed: ' . pg_last_error());
   if (pg_num_rows($result) > 0) submit_error($dbconn, $record3, "already submitted");
   pg_free_result($result);
 
@@ -97,10 +132,10 @@ if ($confirmid) {
   }
 
   if ($parfile)
-    $result = pg_query_params($dbconn, "UPDATE submissions2 SET confidence=confidence+1, subcount=subcount+1, s3=false, hasparity=true, parity=$1, crc32=$2, trackcrcs=$3 WHERE id=$4 AND tocid=$5", array($paritysample, $crc32, $trackcrcs, $confirmid, $tocid));
+    $result = pg_query_params($dbconn, "UPDATE submissions2 SET confidence=confidence+1, subcount=subcount+1, s3=false, hasparity=true, parity=$1, syndrome=$6, crc32=$2, trackcrcs=$3 WHERE id=$4 AND tocid=$5", array($paritysample, $crc32, $trackcrcs, $confirmid, $tocid, $syndromesample));
   else
     $result = pg_query_params($dbconn, "UPDATE submissions2 SET confidence=confidence+1, subcount=subcount+1 WHERE id=$1 AND tocid=$2", array($confirmid, $tocid));
-  $result or die('Query failed: ' . pg_last_error());
+  $result or fatal_error('Query failed: ' . pg_last_error());
   if (pg_affected_rows($result) > 1) submit_error($dbconn, $record3, "not unique");
   if (pg_affected_rows($result) < 1) submit_error($dbconn, $record3, "not found");
   pg_free_result($result);
@@ -108,7 +143,7 @@ if ($confirmid) {
 } else
 {
   $result = pg_query_params($dbconn, "SELECT * FROM submissions2 WHERE tocid=$1 AND crc32=$2 AND trackoffsets=$3", array($tocid, $crc32, $toc['trackoffsets']))
-    or die('Query failed: ' . pg_last_error());
+    or fatal_error('Query failed: ' . pg_last_error());
   if (pg_num_rows($result) > 0) submit_error($dbconn, $record3, "duplicate entry"); // or confirm?
   pg_free_result($result);
 
@@ -121,6 +156,7 @@ if ($confirmid) {
   $record['trackcrcs'] = $trackcrcs;
   $record['confidence'] = $record3['confidence'];
   $record['parity'] = $paritysample;
+  $record['syndrome'] = $syndromesample;
   $record['artist'] = @$_POST['artist'];
   $record['title'] = @$_POST['title'];
   $record['tocid'] = $tocid;
@@ -133,26 +169,26 @@ if ($confirmid) {
   pg_free_result($result);
 
   pg_insert($dbconn, 'submissions2', $record)
-    or die('Query failed');
+    or fatal_error('Query failed');
 
   $record3['entryid'] = $record['id'];
 }
 
 pg_insert($dbconn, 'submissions', $record3)
-  or die('Query failed');
+  or fatal_error('Query failed');
 
 if ($parfile)
 {
   //@mkdir($target_path, 0777, true);
   $parfilename = 'parity/' . $record3['entryid'];
   move_uploaded_file($tmpname, $parfilename)
-    or die('error uploading file ' . $tmpname . ' to ' . $parfilename);
+    or fatal_error('error uploading file ' . $tmpname . ' to ' . $parfilename);
 }
 
 if ($confirmid)
-  printf("%s has been confirmed", $tocid);
+  report_success(sprintf("%s has been confirmed", $tocid));
 else if ($parfile)
-  printf("%s has been uploaded", $tocid);
+  report_success(sprintf("%s has been uploaded", $tocid));
 else
-  printf("%s has been submitted", $tocid);
+  report_success(sprintf("%s has been submitted", $tocid));
 ?>
