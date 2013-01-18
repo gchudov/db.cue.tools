5d8608a02c59e5459e2219381b9b2d569deaafdb
Gregory S. Chudov <Gregory S. Chudov@localhost>
2013-01-18 04:17:49 +0000

    discogs conversion: use postgresql9 and s3fuse

--- utils/discogs/create_db.sh
+++ utils/discogs/create_db.sh
@@ -9,6 +9,6 @@ for table in artist_credit_name artist_credit artist_name video label release re
   gunzip -c discogs_"$table"_sql.gz | psql -U $dbuser -d $dbname
 done
 psql -U $dbuser -d $dbname -f $(dirname $0)/create_keys.sql
-psql -U postgres -d $dbname -f /usr/share/pgsql/contrib/cube.sql
+psql -U postgres -d $dbname -c "CREATE EXTENSION cube"
 psql -U $dbuser -d $dbname -f $(dirname $0)/create_cube.sql
 pg_dump -Fc -U postgres $dbname > discogs.bin
