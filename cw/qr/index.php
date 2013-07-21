d1a921da762a21947d989fa74d257017d234fb8b
Gregory S. Chudov <Gregory S. Chudov@localhost>
2013-07-21 18:28:11 +0000

    client-side cache for coinwidget data

--- cw/qr/index.php
+++ cw/qr/index.php
@@ -1,15 +1,28 @@
 <?php
 
-$apc_id = md5($_SERVER['REQUEST_URI']);
-if (apc_exists($apc_id)) {
+function finish($body, $maxage)
+{
+    $etag = md5($body);
     Header("Content-type: image/png");
-    $image = apc_fetch($apc_id);
-    die($image);
+    header("Cache-Control: max-age=" . $maxage);
+    header("ETag:  " . $etag);
+    if (@$_SERVER['HTTP_IF_NONE_MATCH'] == $etag) {
+        header($_SERVER["SERVER_PROTOCOL"]." 304 Not Modified");
+        exit;
+    }
+    die($body);
 }
+
+$maxage = 3600;
+$apc_id = md5(getlastmod() + $_SERVER['REQUEST_URI']);
+if (apc_exists($apc_id))
+    finish(apc_fetch($apc_id), $maxage);
  
 include "qrlib.php";
 $address = empty($_GET['address'])?'':$_GET['address'];
 ob_start();
 QRcode::png($address);
-apc_store($apc_id, ob_get_contents(), 3600);
-ob_end_flush();
+$body = ob_get_contents();
+ob_end_clean();
+apc_store($apc_id, $body, $maxage);
+finish($body, $maxage);
