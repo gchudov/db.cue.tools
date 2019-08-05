d8e9992f1de88042ccb921a08b30bc188beb5435
gchudov <gchudov@gmail.com>
2019-08-05 17:19:16 +0000

    move freedb database into postgres docker container

--- utils/freedb/import.sh
+++ utils/freedb/import.sh
@@ -1,13 +1,14 @@
 dbname=freedb1
+dbmaster=postgres1
 dbuser=freedb_user
-psql -U postgres -c "DROP DATABASE $dbname"
-psql -U postgres -c "CREATE DATABASE $dbname"
-psql -U postgres -c "ALTER DATABASE $dbname OWNER TO $dbuser"
-#psql -U postgres -d $dbname -f /usr/share/pgsql/contrib/cube.sql
-psql -U postgres -d $dbname -c "CREATE EXTENSION cube"
-psql -U $dbuser -d $dbname -f $(dirname $0)/create_tables.sql
+dbhost=localhost
+dbport=6544
+psql -U postgres -h $dbhost -p $dbport -d $dbmaster -c "DROP DATABASE $dbname"
+psql -U postgres -h $dbhost -p $dbport -d $dbmaster -c "CREATE DATABASE $dbname OWNER $dbuser"
+psql -U postgres -h $dbhost -p $dbport -d $dbname -c "CREATE EXTENSION cube"
+psql -U $dbuser -h $dbhost -p $dbport -d $dbname -f $(dirname $0)/create_tables.sql
 for table in artist_names genre_names entries tracks ; do
-  s3cmd --no-progress get s3://private.cuetools.net/freedb/`date +%Y%m`01/freedb_"$table".sql.bz2 - | nice -n 19 bunzip2 | psql -U $dbuser -d $dbname
+  s3cmd --no-progress get s3://private.cuetools.net/freedb/`date +%Y%m`01/freedb_"$table".sql.bz2 - | nice -n 19 bunzip2 | psql -U $dbuser -h $dbhost -p $dbport -d $dbname
 done
-psql -U $dbuser -d $dbname -f $(dirname $0)/create_keys.sql
-psql -U postgres -d $dbname -c "VACUUM"
+psql -U $dbuser -h $dbhost -p $dbport -d $dbname -f $(dirname $0)/create_keys.sql
+psql -U postgres -h $dbhost -p $dbport -d $dbname -c "VACUUM"
--- utils/freedb/switch.sh
+++ utils/freedb/switch.sh
@@ -1,5 +1,5 @@
-service pgbouncer stop
-psql -U postgres -c "DROP DATABASE freedb2"
-psql -U postgres -c "ALTER DATABASE freedb RENAME TO freedb2"
-psql -U postgres -c "ALTER DATABASE freedb1 RENAME TO freedb"
-service pgbouncer start
+docker stop pgbouncer
+docker exec -it postgres psql -U postgres -c "DROP DATABASE freedb2"
+docker exec -it postgres psql -U postgres -c "ALTER DATABASE freedb RENAME TO freedb2"
+docker exec -it postgres psql -U postgres -c "ALTER DATABASE freedb1 RENAME TO freedb"
+docker start pgbouncer
