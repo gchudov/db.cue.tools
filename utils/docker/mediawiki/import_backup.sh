2cf0f80a0fed053010f729da58c33fa4050cca5d
root <root@ip-10-0-0-250.ec2.internal>
2021-02-27 19:22:05 +0000

    Move php code to the docker container using it

--- utils/docker/mediawiki/import_backup.sh
+++ utils/docker/mediawiki/import_backup.sh
@@ -2,6 +2,7 @@ latest=`aws s3 cp --quiet s3://backups.cuetools.net/LATEST -`
 aws s3 cp --quiet "s3://backups.cuetools.net/$latest/ctwiki-images.tar" /tmp/
 docker run --rm --volumes-from ctwiki -v /tmp/ctwiki-images.tar:/backup/ctwiki-images.tar ubuntu bash -c "cd /var/www/html/images && tar xf /backup/ctwiki-images.tar ."
 dbname=ctwiki
+dbschema=mediawiki
 dbuser=ctwiki
 dbmaster=postgres
 dbcont=postgres96
@@ -11,8 +12,9 @@ $psql -U postgres -d $dbmaster -c "DROP DATABASE $dbname"
 $psql -U postgres -d $dbmaster -c "CREATE ROLE $dbuser LOGIN"
 $psql -U postgres -d $dbmaster -c "CREATE DATABASE $dbname OWNER $dbuser"
 aws s3 cp --quiet "s3://backups.cuetools.net/$latest/ctwiki.bin" - | $pg_restore -U postgres -d $dbname --no-owner
-$psql -U postgres -d $dbname -c "GRANT ALL ON SCHEMA mediawiki TO $dbuser"
-$psql -U postgres -d $dbname -c "GRANT ALL ON ALL TABLES IN SCHEMA mediawiki TO $dbuser"
+$psql -U postgres -d $dbname -c "GRANT ALL ON SCHEMA $dbschema TO $dbuser"
+$psql -U postgres -d $dbname -c "GRANT ALL ON ALL TABLES IN SCHEMA $dbschema TO $dbuser"
+$psql -U postgres -d $dbname -c "GRANT ALL ON ALL SEQUENCES IN SCHEMA $dbschema TO $dbuser"
 $psql -U postgres -d $dbname -c "VACUUM"
 $psql -U postgres -d $dbname -c "ANALYZE"
 rm "/tmp/ctwiki-images.tar"
