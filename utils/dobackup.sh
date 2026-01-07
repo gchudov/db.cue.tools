413ac801bbd94668a0449d7e9e07e404269c3634
Grigory Chudov <gchudov@gmail.com>
2026-01-07 05:30:51 +0000

    pgbouncer shouldn't log too much

--- utils/dobackup.sh
+++ utils/dobackup.sh
@@ -5,7 +5,7 @@ docker exec postgres16 pg_dump -Z 3 -Fc ctdb -U postgres > $backup_path/ctdb.bin
 docker exec postgres16 pg_dump -Z 3 -Fc ctwiki -U postgres > $backup_path/ctwiki.bin
 #tar --gzip --create --file="$backup_path/ctdb.tgz" /opt/ctdb/www/ctdbweb --exclude="/opt/db.cue.tools/parity/*"
 docker run --rm --volumes-from ctwiki -v $backup_path:/backup ubuntu bash -c "cd /var/www/html/images && tar cf /backup/ctwiki-images.tar ."
-aws s3 sync --quiet --storage-class REDUCED_REDUNDANCY $backup_path/ s3://backups.cuetools.net/$s3_path/
+aws s3 sync --quiet --storage-class GLACIER_IR $backup_path/ s3://backups.cuetools.net/$s3_path/
 echo -n "$s3_path" > $backup_path/LATEST
 aws s3 cp --quiet $backup_path/LATEST s3://backups.cuetools.net/
 rm -rf $backup_path/*
